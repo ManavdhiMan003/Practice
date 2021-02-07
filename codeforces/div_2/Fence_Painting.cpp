5ffdcec26cac6d317a529adeae729cc212bfacb0
manavdhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2021-02-07 15:31:48 +0530

    div 2 699

--- codeforces/div_2/Fence_Painting.cpp
+++ codeforces/div_2/Fence_Painting.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define endl "\n"
-#define loop(i,n) for(i=0;i<n;i++)
+#define loop(i,n) for(ll i=0;i<n;i++)
 #define loop1(i,k,n) for(i=k;i<n;i++)
 #define pb push_back
 typedef long long int ll;
@@ -21,43 +21,68 @@ int main(){
 	cin.tie(NULL);
     ll t,n,m;
     cin>>t;
+    vector<ll> g[100000];
     while(t--){
         cin>>n>>m;
-        vector<ll> a(n);
-        for(ll i=0;i<n;i++){
+        for(ll i=1;i<=n;i++){
+            g[i].clear();
+        }
+        vector<ll> a(n+1);
+        for(ll i=1;i<=n;i++){
             cin>>a[i];
         }
-        bool ps=true;
-        vector<ll> b(n);
-        map<ll,ll> mp;
-        for(ll i=0;i<n;i++){
+        ll temp;
+        vector<ll> b(n+1);
+        for(ll i=1;i<=n;i++){
             cin>>b[i];
-            if(ps) if(b[i]!=a[i]) ps=false;
-            if(b[i]!=a[i]) mp[i]=b[i];
+            if(b[i]!=a[i]){
+                g[b[i]].pb(i);
+            }
         }
-        if(ps){
-            vector<ll> c(m);
-            bool pst=true;
-            ll id;
-            for(ll i=0;i<m;i++){
-                cin>>c[i];
-                if(pst){
-                    auto it=find(a.begin(),a.end(),c[i]);
-                    if(it!=a.end()){
-                        id=i;
-                        pst=false;
-                    }
-                }
-            }
-            if(pst) cout<<"NO\n";
-            else{
-                cout<<"YES\n";
-                for(ll i=0;i<m;i++) cout<<id+1<<" ";
-                cout<<endl;
-            }
+        vector<ll> c(m+1);
+        for(ll i=1;i<=m;i++) cin>>c[i];
+        ll last=-1;
+        if(g[c[m]].size()>0){
+            last=g[c[m]].back();
+            // cout<<last<<" hhd "<<endl;
+            g[c[m]].pop_back();
         }
         else{
-            cout<<"process\n";
+            for(ll i=1;i<=n;i++){
+                if(b[i]==c[m]){
+                    last=i;
+                    break;
+                }
+            }
+        }
+        if(last==-1){
+            cout<<"NO\n";
+            continue;
+        }
+        vector<ll> ans(m+1);
+        ans[m]=last; 
+        // cout<<a[m]<<" hha ";
+        for(ll i=1;i<m;i++){
+            if(g[c[i]].size()==0) ans[i]=last;
+            else{
+                ans[i]=g[c[i]].back();
+                g[c[i]].pop_back();
+            }
+        }
+        bool ps=true;
+        for(ll i=1;i<=n;i++){
+            if(g[i].size()>0){
+                cout<<"NO\n";
+                ps=false;
+                break;
+            }
+        }
+        if(ps){
+            cout<<"YES\n";
+            for(ll i=1;i<=m;i++){
+                cout<<ans[i]<<" ";
+            }
+            cout<<endl;     
         }
     }
     return 0;
